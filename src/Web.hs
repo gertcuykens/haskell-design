062c6c1681aa90b547a64b4fe370d0bd64232422
Gert Cuykens <gert.cuykens@gmail.com>
2012-08-05 20:23:55 +0200

    web

--- src/Web.hs
+++ src/Web.hs
@@ -1,18 +1,15 @@
-{-# LANGUAGE OverloadedStrings, ScopedTypeVariables #-}
 module Web (webServer) where
 
---import Control.Applicative ((<$>), optional)
---import Data.Maybe (fromMaybe)
---import Data.Text (Text)
---import Data.Text.Lazy (unpack)
-import Happstack.Lite
---import Text.Blaze.Html5 (Html, (!), a, form, input, p, toHtml, label)
---import Text.Blaze.Html5.Attributes (action, enctype, href, name, size, type_, value)
---import qualified Text.Blaze.Html5 as H
---import qualified Text.Blaze.Html5.Attributes as A
+import Happstack.Server
+
+conf :: Conf
+conf = Conf { port      = 8000
+            , validator = Nothing
+            , logAccess = Just logMAccess
+            , timeout   = 30}
 
 fileServing :: ServerPart Response
-fileServing = serveDirectory EnableBrowsing ["state.htm"] "../www"
+fileServing = serveDirectory EnableBrowsing ["state.htm"] "www"
 
 webServer :: IO ()
-webServer = serve Nothing fileServing
+webServer = simpleHTTP nullConf fileServing
