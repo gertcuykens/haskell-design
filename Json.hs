543184b0dca33bee489499cbf5f71b3242615c2f
Gert Cuykens <gert.cuykens@gmail.com>
2012-11-10 07:30:11 +0100

    json

--- Json.hs
+++ Json.hs
@@ -3,35 +3,35 @@ module Json (AcidState, KeyValue, read', write', open', close') where
 
 import Control.Applicative ((<$>), (<*>))
 import Control.Monad (mzero)
-import Control.Monad.State (get, put)
-import Control.Monad.Reader (ask)
+import Control.Lens (Simple, Iso, iso, (?=), at, from, makeIso)
+import qualified Control.Lens as LENS (query)
 import Data.Maybe (fromMaybe)
 import Data.Aeson ((.:), (.=), Value(Object), FromJSON(parseJSON), ToJSON(toJSON), object, decode)
 import Data.Aeson.Encode (fromValue)
 import Data.Text.Lazy.Builder (toLazyText)
 import Data.Text.Lazy.Internal (Text)
 import Data.Text.Lazy.Encoding (encodeUtf8)
-import Data.Acid (AcidState, Update, Query, update, query, openLocalStateFrom, closeAcidState, makeAcidic)
+import Data.Acid (AcidState, Update, Query, query, update, openLocalStateFrom, closeAcidState, makeAcidic)
 import Data.SafeCopy (deriveSafeCopy, base)
 import Data.Typeable (Typeable)
 import qualified Data.Map as Map (Map, lookup, insert, empty)
 
 type Key = String
-data User = User Text Text Text Text deriving (Typeable)
-data KeyValue = KeyValue !(Map.Map Key User) deriving (Typeable)
+data User = User Text Text Text Text deriving Typeable
+newtype KeyValue = KeyValue { getKeyValue :: Map.Map Key User }
 
 $(deriveSafeCopy 0 'base ''User)
 $(deriveSafeCopy 0 'base ''KeyValue)
 
+-- $(makeIso ''KeyValue)
+keyValue :: Simple Iso (Map.Map Key User) KeyValue
+keyValue = iso KeyValue getKeyValue
+
 insertKey :: Key -> User -> Update KeyValue ()
-insertKey k v = do
-    KeyValue m <- get
-    put (KeyValue (Map.insert k v m))
+insertKey k v = from keyValue . at k ?= v
 
 lookupKey :: Key -> Query KeyValue (Maybe User)
-lookupKey k = do
-    KeyValue m <- ask
-    return (Map.lookup k m)
+lookupKey k = LENS.query (from keyValue . at k)
 
 $(makeAcidic ''KeyValue ['insertKey, 'lookupKey])
 
@@ -68,3 +68,17 @@ open' = openLocalStateFrom "data/KeyValue" (KeyValue Map.empty)
 close' :: AcidState KeyValue -> IO ()
 close' = closeAcidState
 
+{-
+import Control.Monad.State (get, put)
+import Control.Monad.Reader (ask)
+
+insertKey :: Key -> User -> Update KeyValue ()
+insertKey k v = do
+    KeyValue m <- get
+    put (KeyValue (Map.insert k v m))
+
+lookupKey :: Key -> Query KeyValue (Maybe User)
+lookupKey k = do
+    KeyValue m <- ask
+    return (Map.lookup k m)
+-}
