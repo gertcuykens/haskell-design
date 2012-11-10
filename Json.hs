786c34174794d4e05d0dceda13ba2e30ba7fea44
Gert Cuykens <gert.cuykens@gmail.com>
2012-11-10 01:22:28 +0100

    json

--- Json.hs
+++ Json.hs
@@ -1,22 +1,20 @@
 {-# LANGUAGE OverloadedStrings, DeriveDataTypeable, TypeFamilies, TemplateHaskell #-}
 module Json (AcidState, KeyValue, read', write', open', close') where
 
-import Control.Applicative
---import Control.Monad.IO.Class (MonadIO, liftIO)
+import Control.Applicative ((<$>), (<*>))
 import Control.Monad (mzero)
 import Control.Monad.State (get, put)
 import Control.Monad.Reader (ask)
 import Data.Maybe (fromMaybe)
-import Data.Aeson hiding (Value)
+import Data.Aeson ((.:), (.=), Value(Object), FromJSON(parseJSON), ToJSON(toJSON), object, decode)
 import Data.Aeson.Encode (fromValue)
---import Data.Text.Lazy (toStrict)
 import Data.Text.Lazy.Builder (toLazyText)
 import Data.Text.Lazy.Internal (Text)
 import Data.Text.Lazy.Encoding (encodeUtf8)
 import Data.Acid (AcidState, Update, Query, update, query, openLocalStateFrom, closeAcidState, makeAcidic)
 import Data.SafeCopy (deriveSafeCopy, base)
 import Data.Typeable (Typeable)
-import qualified Data.Map as Map
+import qualified Data.Map as Map (Map, lookup, insert, empty)
 
 type Key = String
 data User = User Text Text Text Text deriving (Typeable)
@@ -30,7 +28,7 @@ instance FromJSON User where
                                 <*> v .: "country"
                                 <*> v .: "phone"
                                 <*> v .: "email"
-    parseJSON _          = mzero
+    parseJSON _ = mzero
 
 instance ToJSON User where
     toJSON (User a b c d)= object ["city"    .= a
@@ -50,23 +48,19 @@ lookupKey k = do
 
 $(makeAcidic ''KeyValue ['insertKey, 'lookupKey])
 
-text :: User -> Text
-text = toLazyText . fromValue . toJSON
-
-user:: Text -> Maybe User
-user =  decode . encodeUtf8
-
 read' :: AcidState KeyValue -> String -> IO Text
 read' s' k = do
     u' <- query s' (LookupKey k)
     case u' of
-        Just u ->return $ text u
-        Nothing -> return $ text (User "" "" "" "")
+        Just u ->return $ f u
+        Nothing -> return $ f (User "" "" "" "")
+        where f = toLazyText . fromValue . toJSON
 
 write' :: AcidState KeyValue -> String -> Text -> IO ()
 write' s' k v = do
-    let u = fromMaybe (error "invalid json") (user v)
+    let u = fromMaybe (error "invalid json") (f v)
     update s' (InsertKey k u)
+    where f = decode . encodeUtf8
 
 open' :: IO (AcidState KeyValue)
 open' = openLocalStateFrom "data/KeyValue" (KeyValue Map.empty)
