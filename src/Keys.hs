cd9840192657d8f9ead4735c6d9481dbb3e3924f
Gert Cuykens <gert.cuykens@gmail.com>
2013-02-09 01:59:03 +0100

    Update src/Keys.hs

--- src/Keys.hs
+++ src/Keys.hs
@@ -4,18 +4,17 @@ import Network.OAuth.OAuth2 (OAuth2(..))
 
 googleKey :: OAuth2
 googleKey = OAuth2 { oauthClientId = ""
-                   , oauthClientSecret = ""
-                   --, oauthCallback = Just "https://developers.google.com/oauthplayground"
-                   , oauthCallback = Just "http://localhost:9160/code.htm"
-                   , oauthOAuthorizeEndpoint = "https://accounts.google.com/o/oauth2/auth"
-                   , oauthAccessTokenEndpoint = "https://accounts.google.com/o/oauth2/token"
-                   , oauthAccessToken = Nothing}
+                   , oauthClientSecret = ""
+                   --, oauthCallback = Just "https://developers.google.com/oauthplayground"
+                   , oauthCallback = Just "https://localhost:9160/code.htm"
+                   , oauthOAuthorizeEndpoint = "https://accounts.google.com/o/oauth2/auth"
+                   , oauthAccessTokenEndpoint = "https://accounts.google.com/o/oauth2/token"
+                   , oauthAccessToken = Nothing}
 
 facebookKey :: OAuth2
 facebookKey = OAuth2 { oauthClientId = ""
-                      , oauthClientSecret = ""
-                      , oauthCallback = Just "http://localhost:9160/code.htm"
-                      , oauthOAuthorizeEndpoint = "https://www.facebook.com/dialog/oauth"
-                      , oauthAccessTokenEndpoint = "https://graph.facebook.com/oauth/access_token"
-                      , oauthAccessToken = Nothing}
-
+                      , oauthClientSecret = ""
+                      , oauthCallback = Just "https://localhost:9160/code.htm"
+                      , oauthOAuthorizeEndpoint = "https://www.facebook.com/dialog/oauth"
+                      , oauthAccessTokenEndpoint = "https://graph.facebook.com/oauth/access_token"
+                      , oauthAccessToken = Nothing}
