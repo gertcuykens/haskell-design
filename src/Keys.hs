5dc0b2c35fa7b8b94dcb7830945595651a46d19e
Gert Cuykens <gert.cuykens@gmail.com>
2013-08-10 04:39:25 +0200

    key

--- src/Keys.hs
+++ src/Keys.hs
@@ -3,18 +3,16 @@ module Keys where
 import Network.OAuth.OAuth2 (OAuth2(..))
 
 googleKey :: OAuth2
-googleKey = OAuth2 { oauthClientId = "522156758812-09f5qv0e4gqjdjqfocerqcud5m5jutau.apps.googleusercontent.com"
+googleKey = OAuth2 { oauthClientId = ""
                    , oauthClientSecret = ""
-                   --, oauthCallback = Just "https://developers.google.com/oauthplayground"
                    , oauthCallback = Just "http://localhost:9160"
                    , oauthOAuthorizeEndpoint = "https://accounts.google.com/o/oauth2/auth"
-                   , oauthAccessTokenEndpoint = "https://accounts.google.com/o/oauth2/token"
-                   , oauthAccessToken = Nothing}
+                   , oauthAccessTokenEndpoint = "https://accounts.google.com/o/oauth2/token"}
 
 facebookKey :: OAuth2
 facebookKey = OAuth2 { oauthClientId = ""
                      , oauthClientSecret = ""
                      , oauthCallback = Just "http://localhost:9160"
                      , oauthOAuthorizeEndpoint = "https://www.facebook.com/dialog/oauth"
-                     , oauthAccessTokenEndpoint = "https://graph.facebook.com/oauth/access_token"
-                     , oauthAccessToken = Nothing}
+                     , oauthAccessTokenEndpoint = "https://graph.facebook.com/oauth/access_token"}
+
